5386494d3c6ec7f4f25b363033e578ef2f936dee
Jayanta garu <jayanta.garu@gmail.com>
2021-07-22 20:12:03 +0530

    commit

--- Basic/Crypt/cc.js
+++ Basic/Crypt/cc.js
@@ -9,4 +9,5 @@ let new_CC_No = bcrypt.hashSync(user_Payment.cc_No, 10);
 let new_cvv = bcrypt.hashSync(user_Payment.cvv, 10);
 
 let new_User_Payment = { ...user_Payment, cc_No: new_CC_No, cvv: new_cvv };
+
 console.log(new_User_Payment);
